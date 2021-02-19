8720976438fe64e1650e4da508d6706e9380c188
esriis <erlend.s.riis@gmail.com>
2021-02-19 09:27:26 +0000

    Update urls.py

--- azuresite/urls.py
+++ azuresite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from blobdatabase.views import update_view
 
 urlpatterns = [
+    path('update',update_view),
     path('', include('blobdatabase.urls')),
     path('admin/', admin.site.urls),
 ]
