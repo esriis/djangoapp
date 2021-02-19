66b2c02777fdbd6f840f6898258503e8d4c471d2
esriis <erlend.s.riis@gmail.com>
2021-02-19 09:41:10 +0000

    Update views.py

--- blobdatabase/views.py
+++ blobdatabase/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from functions.readTable import readTable
+from pathlib import Path
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the blobstore index.")
 
 def update_view(request):
-    readTable()
-    return HttpResponse("Welcome to my world")
+    tablePath = Path("../input/tableIn.csv")
+    if tablePath.is_file():
+        readTable()
+        return HttpResponse("Updated")
+    else:
+        return HttpResponse("No file found")
