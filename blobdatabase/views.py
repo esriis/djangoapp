cf1dad092ca839cd4fe5c248ab91d2ad699075dd
esriis <erlend.s.riis@gmail.com>
2021-02-19 09:14:31 +0000

    Update views.py

--- blobdatabase/views.py
+++ blobdatabase/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from functions.readTable import readTable
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the newApp index.")
+    return HttpResponse("Hello, world. You're at the blobstore index.")
+
+def update_view(request):
+    readTable()
+    return HttpResponse("Welcome to my world")
