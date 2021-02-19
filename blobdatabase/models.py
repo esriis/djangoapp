bceea2f6aae4780784d33719bfc74b5c84a1a322
esriis <erlend.s.riis@gmail.com>
2021-02-19 09:12:55 +0000

    Update models.py

--- blobdatabase/models.py
+++ blobdatabase/models.py
@@ -4,13 +4,15 @@ from django.db import models
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,unique=True)
     def __str__(self):
         return self.name 
     
 class Building(models.Model):
     name = models.CharField(max_length=100)
     client = models.ForeignKey(Client,on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ('name', 'client',)
     def __str__(self):
         return self.name 
     
@@ -18,20 +20,21 @@ class Building(models.Model):
 class Project(models.Model):
     name = models.CharField(max_length=100)
     building = models.ForeignKey(Building,on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ('name', 'building')
     def __str__(self):
-        return self.name 
+        return self.building.name + ": " + self.name 
     
 
 class Blob(models.Model):
     name = models.CharField(max_length=200)
-    fullPath = models.CharField(max_length=400)
+    fullPath = models.CharField(max_length=400,unique=True)
     project = models.ForeignKey(Project,on_delete=models.CASCADE)
     modifiedDate = models.DateTimeField('date modified')
     createdDate = models.DateTimeField('date created')
     modifiedBy = models.CharField(max_length=100)
     createdBy = models.CharField(max_length=100)
-    guid = models.CharField(max_length=50)
+    guid = models.CharField(max_length=50,unique=True)
     extension = models.CharField(max_length=30)
     def __str__(self):
-        return self.name 
-    
+        return self.fullPath 
