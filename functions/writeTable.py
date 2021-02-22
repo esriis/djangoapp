0614bc71b24e043b3bf759d722d777b9617ee527
esriis <erlend.s.riis@gmail.com>
2021-02-22 12:51:25 +0000

    Update writeTable.py

--- functions/writeTable.py
+++ functions/writeTable.py
@@ -9,33 +9,34 @@ Created on Fri Feb 19 13:56:18 2021
 
 import csv
 from blobdatabase.models import Blob
+from django.http import HttpResponse
 
 
 def writeTable():
     
-    filename = "tables/tableOut.csv"
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="tableOut.csv"'
     
-    dateformat = "%d/%m/%y %H:%M:%S"
+    dateformat = "%d/%m/%Y %H:%M:%S"
 
     blobList = Blob.objects.all()        
-    with open(filename, 'w', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(['client','building','project','name','fullPath',
-                              'modifiedDate','createdDate','modifiedBy',
-                              'createdBy','guid','extension'])
-        
-        for blob in blobList:
-            spamwriter.writerow([blob.project.building.client,
-                                  blob.project.building,
-                                  blob.project.name,
-                                  blob.name,
-                                  blob.fullPath,
-                                  blob.modifiedDate.strftime(dateformat),
-                                  blob.createdDate.strftime(dateformat),
-                                  blob.modifiedBy,
-                                  blob.createdBy,
-                                  blob.guid,
-                                  blob.extension])
+    writer = csv.writer(response, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['client','building','project','name','fullPath',
+                          'modifiedDate','createdDate','modifiedBy',
+                          'createdBy','guid','extension'])
+    
+    for blob in blobList:
+        writer.writerow([blob.project.building.client,
+                              blob.project.building,
+                              blob.project.name,
+                              blob.name,
+                              blob.fullPath,
+                              blob.modifiedDate.strftime(dateformat),
+                              blob.createdDate.strftime(dateformat),
+                              blob.modifiedBy,
+                              blob.createdBy,
+                              blob.guid,
+                              blob.extension])
             
-
+    return response
