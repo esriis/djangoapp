91dd2afe7347a2b079fd95140bb9e083aabe133e
esriis <erlend.s.riis@gmail.com>
2021-02-24 07:58:13 +0000

    Update views.py

--- blobdatabase/views.py
+++ blobdatabase/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from functions.tableService import updateTable, deleteTable, writeTable
+from functions.tableService import uploadTable, deleteTable, writeTable
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 # from .forms import UploadFileForm
@@ -39,7 +39,7 @@ def upload_view(request):
             # print(form.title)
             # print(form.file)
             # if form.is_valid():
-            response = updateTable(request.FILES['file'])
+            response = uploadTable(request.FILES['file'],decode=True)
             # else:
             #     response = "Uh oh."
             logout(request)
@@ -63,12 +63,59 @@ def delete_view(request):
             # print(form.title)
             # print(form.file)
             # if form.is_valid():
-            response = deleteTable(request.FILES['file'])
+            response = deleteTable(request.FILES['file'],decode=True)
             # else:
             #     response = "Uh oh."
             logout(request)
             return HttpResponse(response)
         else:
-            return HttpResponse("Login unsuccessful! Username: " + username + ". Pw: " + password)
+            return HttpResponse("Login unsuccessful.")
+    else:
+        return HttpResponse("Requires POST")
+    
+
+@csrf_exempt
+def deleteFTP_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            path = "../tables/deleteTable.csv"
+            if Path(path).is_file():
+                response = deleteTable(open(path))
+            else:
+                response = "File was not found."
+            # else:
+            #     response = "Uh oh."
+            logout(request)
+            return HttpResponse(response)
+        else:
+            return HttpResponse("Login unsuccessful.")
+    else:
+        return HttpResponse("Requires POST")
+    
+    
+    
+@csrf_exempt
+def uploadFTP_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            path = "../tables/uploadTable.csv"
+            if Path(path).is_file():
+                response = uploadTable(open(path))
+            else:
+                response = "File was not found."
+            # else:
+            #     response = "Uh oh."
+            logout(request)
+            return HttpResponse(response)
+        else:
+            return HttpResponse("Login unsuccessful.")
     else:
         return HttpResponse("Requires POST")
