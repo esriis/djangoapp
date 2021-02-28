1a638ea88ca000fc03e694baac22acea7069cb31
esriis <erlend.s.riis@gmail.com>
2021-02-28 15:06:20 +0100

    Update models.py

--- blobdatabase/models.py
+++ blobdatabase/models.py
@@ -8,7 +8,7 @@ class Client(models.Model):
     def __str__(self):
         return self.name 
     
-class Building(models.Model):
+class Project(models.Model):
     name = models.CharField(max_length=100)
     client = models.ForeignKey(Client,on_delete=models.CASCADE)
     class Meta:
@@ -17,26 +17,26 @@ class Building(models.Model):
         return self.name 
     
     
-class Project(models.Model):
-    name = models.CharField(max_length=100)
-    building = models.ForeignKey(Building,on_delete=models.CASCADE)
+class SubProject(models.Model):
+    dataType = models.CharField(max_length=100)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
     class Meta:
-        unique_together = ('name', 'building')
+        unique_together = ('dataType', 'project')
     def __str__(self):
-        return self.building.name + ": " + self.name 
+        return self.project.name + ", " + self.dataType 
     
 
 class Blob(models.Model):
     name = models.CharField(max_length=200)
     fullPath = models.CharField(max_length=400,unique=True)
-    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    subProject = models.ForeignKey(SubProject,on_delete=models.CASCADE)
     modifiedDate = models.DateTimeField('date modified')
-    createdDate = models.DateTimeField('date created')
+    # createdDate = models.DateTimeField('date created')
     modifiedBy = models.CharField(max_length=100)
-    createdBy = models.CharField(max_length=100)
+    # createdBy = models.CharField(max_length=100)
     guid = models.CharField(max_length=50,unique=True)
     extension = models.CharField(max_length=30)
     class Meta:
         ordering = ['guid']
     def __str__(self):
-        return self.fullPath 
+        return self.fullPath
