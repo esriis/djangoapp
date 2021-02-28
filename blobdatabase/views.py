8fcd2670931965dc0af5e9258c33147f587f80e8
esriis <erlend.s.riis@gmail.com>
2021-02-28 16:08:37 +0100

    Update views.py

--- blobdatabase/views.py
+++ blobdatabase/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from functions.tableService import uploadTable, deleteTable, writeTable
+from functions.tableService import uploadTable, deleteTable, writeTable
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 from pathlib import Path
@@ -19,7 +19,7 @@ def download_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-  #          response = writeTable()
+            response = writeTable()
             logout(request)
             return(response)
         else:
@@ -36,13 +36,7 @@ def upload_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # form = UploadFileForm(request.POST, request.FILES)
-            # print(form.title)
-            # print(form.file)
-            # if form.is_valid():
-  #          response = uploadTable(request.FILES['file'],decode=True)
-            # else:
-            #     response = "Uh oh."
+            response = uploadTable(request.FILES['file'],decode=True)
             logout(request)
             return HttpResponse(response)
         else:
@@ -60,13 +54,7 @@ def delete_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # form = UploadFileForm(request.POST, request.FILES)
-            # print(form.title)
-            # print(form.file)
-            # if form.is_valid():
-  #          response = deleteTable(request.FILES['file'],decode=True)
-            # else:
-            #     response = "Uh oh."
+            response = deleteTable(request.FILES['file'],decode=True)
             logout(request)
             return HttpResponse(response)
         else:
@@ -85,12 +73,10 @@ def deleteFTP_view(request):
             login(request, user)
             path = "tables/deleteTable.csv"
             if Path(path).is_file():
-  #              response = deleteTable(open(path,encoding='utf-8-sig'))
+                response = deleteTable(open(path,encoding='utf-8-sig'))
                 Path(path).unlink()
             else:
                 response = "File not found."
-            # else:
-            #     response = "Uh oh."
             logout(request)
             return HttpResponse(response)
         else:
@@ -110,12 +96,10 @@ def uploadFTP_view(request):
             login(request, user)
             path = "tables/uploadTable.csv"
             if Path(path).is_file():
- #               response = uploadTable(open(path,encoding='utf-8-sig'))
+                response = uploadTable(open(path,encoding='utf-8-sig'))
                 Path(path).unlink()
             else:
                 response = "File not found."
-            # else:
-            #     response = "Uh oh."
             logout(request)
             return HttpResponse(response)
         else:
