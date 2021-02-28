bd5a594f962d8b1098bd76de451dcfb169eb4030
esriis <erlend.s.riis@gmail.com>
2021-02-28 15:37:17 +0100

    Update views.py

--- blobdatabase/views.py
+++ blobdatabase/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from functions.tableService import uploadTable, deleteTable, writeTable
+#from functions.tableService import uploadTable, deleteTable, writeTable
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 from pathlib import Path
@@ -19,7 +19,7 @@ def download_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            response = writeTable()
+  #          response = writeTable()
             logout(request)
             return(response)
         else:
@@ -40,7 +40,7 @@ def upload_view(request):
             # print(form.title)
             # print(form.file)
             # if form.is_valid():
-            response = uploadTable(request.FILES['file'],decode=True)
+  #          response = uploadTable(request.FILES['file'],decode=True)
             # else:
             #     response = "Uh oh."
             logout(request)
@@ -64,7 +64,7 @@ def delete_view(request):
             # print(form.title)
             # print(form.file)
             # if form.is_valid():
-            response = deleteTable(request.FILES['file'],decode=True)
+  #          response = deleteTable(request.FILES['file'],decode=True)
             # else:
             #     response = "Uh oh."
             logout(request)
@@ -85,7 +85,7 @@ def deleteFTP_view(request):
             login(request, user)
             path = "tables/deleteTable.csv"
             if Path(path).is_file():
-                response = deleteTable(open(path,encoding='utf-8-sig'))
+  #              response = deleteTable(open(path,encoding='utf-8-sig'))
                 Path(path).unlink()
             else:
                 response = "File not found."
@@ -110,7 +110,7 @@ def uploadFTP_view(request):
             login(request, user)
             path = "tables/uploadTable.csv"
             if Path(path).is_file():
-                response = uploadTable(open(path,encoding='utf-8-sig'))
+ #               response = uploadTable(open(path,encoding='utf-8-sig'))
                 Path(path).unlink()
             else:
                 response = "File not found."
